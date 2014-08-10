ddd7dee4308d4b18ef107fec269154f464fb8945
root <root@local-testserver.(none)>
2014-08-10 04:02:22 -0400

    Updated version in request string

--- _config.php
+++ _config.php
@@ -5,7 +5,7 @@ define('SWC_PASSWORD', '<YOUR PASSWORD HERE>');
 define('LIVE_OUTPUT_ENABLED', true);
 
 define('SLEEP_MILLISECONDS', 750);
-define('GET_SESSION_URL', 'https://sealswithclubs.eu/sealsloginhandler.php?login=X&JSON=1&Version=v0.2.14');
+define('GET_SESSION_URL', 'https://sealswithclubs.eu/sealsloginhandler.php?login=X&JSON=1&Version=v0.2.18');
 define('USER_AGENT_STR', "python-requests/1.1.0 CPython/2.7.3 Windows/7");
 define('SERVICE_URL', 'sealswithclubs.eu');
 define('SERVICE_PORT', 8088);
